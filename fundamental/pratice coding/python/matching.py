7aceda7a2a58536bf7331786fda1e9813b005841
Saifa Decha <Saifadeacha@gmail.com>
2026-06-12 14:00:00 +0700

    feat: add wildcard pattern matching

--- fundamental/pratice coding/python/matching.py
+++ fundamental/pratice coding/python/matching.py
@@ -5,3 +5,11 @@ match service :
     case 2 : print("ถอนเงิน")
     case None: print("Invalid input")
 
+#wildcard pattern ไม่ตรงกับเคสไหนเลย
+service = 1000
+match service :
+    case 1 : print("ฝากเงิน")
+    case 2 : print("ถอนเงิน")
+    case None: print("Invalid input")
+    case _: print("default") #defualt case 
+

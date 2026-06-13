18ad5509ced684b26ae7532766e20ac4e63faf18
Saifa Decha <Saifadeacha@gmail.com>
2026-06-13 11:00:00 +0700

    feat: add capture pattern matching

--- fundamental/pratice coding/python/matching.py
+++ fundamental/pratice coding/python/matching.py
@@ -13,3 +13,11 @@ match service :
     case None: print("Invalid input")
     case _: print("default") #defualt case 
 
+#capture pattern นำ service มาเเจกเเจงรายละเอียด 
+service = 1000
+match service :
+    case 1 : print("ฝากเงิน")
+    case 2 : print("ถอนเงิน")
+    case None: print("Invalid input")
+    case service : print(f"{service} ไม่อยู่ในบริการใน 1 2 None") #เอา service มาใช้ 
+

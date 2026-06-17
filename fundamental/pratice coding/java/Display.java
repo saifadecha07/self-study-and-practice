84220f6eaa86e820f6fa9aac892ff5b26803fedc
Saifa Decha <Saifadeacha@gmail.com>
2026-06-17 18:52:15 +0700

    feat: add java practice files for fundamentals

---
+++ fundamental/pratice coding/java/Display.java
@@ -0,0 +1,7 @@
+public class Display {
+    public static void main(String[] args) {
+        System.out.print("newbie "); // comment right
+        System.out.println("I love you");
+        /* it's worked */
+    }
+}
---
+++ fundamental/pratice coding/java/Variable.java
@@ -0,0 +1,18 @@
+public class Variable {
+    public static void main(String[] args) {
+        // define variable
+        int x = 5,y=10;
+        int a;
+        a = 10;
+        System.out.println(a+x+y+"ddd");
+        double f=5.2f;
+
+        //final constant 
+        final float TAX = 15.54f;
+        System.out.println(TAX);
+        
+
+
+    }
+    
+}

d6da5b3f10d25bec3f2bfb4244a9c1d6b0f957e9
Saifa Decha <Saifadeacha@gmail.com>
2026-06-17 18:52:03 +0700

    feat: add tuple and dictionary practice examples in python

---
+++ fundamental/pratice coding/python/tuple.py
@@ -0,0 +1,13 @@
+i = (1,5,6,4)
+print(i.count(5))
+i = False
+score = 5*int(i)
+
+print(score)
+
+A = 1 if i is False else 0
+print(A)
+colors1 = {"red": "สีแดง", "yellow": "สีเหลือง"}
+colors2 = {"blue": "สีน้ำเงิน", "yellow": "สีทอง"}
+colors1.update(colors2)
+print(colors1["yellow"])

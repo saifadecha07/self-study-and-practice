70b62c696ff111cbe647ad5e9e7049142e2561e8
Saifa Decha <saifadecha07@users.noreply.github.com>
2026-06-20 22:45:11 +0700

    feat: add user input practice using Scanner

---
+++ fundamental/pratice coding/java/Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter your name ");
+        String name = sc.nextLine();
+        System.out.println("Hel\tlow\n\t"+name);
+    }
+    
+}

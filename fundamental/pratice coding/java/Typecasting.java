e12c64420d72233dd9ebb6b2de5955775afe147c
Saifa Decha <saifadecha07@users.noreply.github.com>
2026-06-20 22:47:08 +0700

    feat: add implicit and explicit type casting practice

---
+++ fundamental/pratice coding/java/Typecasting.java
@@ -0,0 +1,13 @@
+public class Typecasting {
+    public static void main(String[] args) {
+        int num = 100000;
+        double nn = num;
+        int in = (int)nn;
+        System.out.println(in);
+        double numD = num;
+        System.out.println(numD);  // small to big it's auto casting by compiler
+
+    }
+    
+    
+}

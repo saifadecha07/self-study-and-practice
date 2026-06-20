9597114670d735813e33a27b2bd32dc5888f8e60
Saifa Decha <saifadecha07@users.noreply.github.com>
2026-06-20 22:47:14 +0700

    feat: add local vs global variable scope and data type limits practice

--- fundamental/pratice coding/java/localGlobal.java
+++ fundamental/pratice coding/java/localGlobal.java
@@ -1,7 +1,16 @@
 public class localGlobal {
-    int a = 1;
-    int b = 2;
     public static void main(String[] args) {
-        System.out.println(a);
+        int b = 10; // global variable work on main
+        System.out.println(b);
+        {
+            int c = 10; //local variable
+            System.out.println(b+c);
+        }
+        if(b==10){ 
+            final int A = 50; // local variable
+            System.out.println(b+A);
+        }
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Character.MAX_VALUE);
     }
 }

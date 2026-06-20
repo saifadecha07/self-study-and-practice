8cf2a71c7a8db422c3d3ae6fd1d441b93e2dd3f4
Saifa Decha <saifadecha07@users.noreply.github.com>
2026-06-20 22:47:03 +0700

    feat: add string concatenation and type parsing practice

---
+++ fundamental/pratice coding/java/String1.java
@@ -0,0 +1,11 @@
+public class String1 {
+    public static void main(String[] args) {
+        String a = "5";
+        String n = " "+a+50 ;
+        System.out.println(n);
+        int num = Integer.parseInt(a);
+        String num1 = ""+num;
+        Double num2 = Double.parseDouble(num1);
+        System.out.println(num2);
+    }
+}

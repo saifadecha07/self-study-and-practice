335d4d8cde4412ab3cc2833d331e09997b6a942b
Saifa Decha <saifadecha07@users.noreply.github.com>
2026-06-28 20:20:01 +0700

    feat: add main_2 demo for Person and Ironman usage
    Demonstrates Person (Student subclass) and Ironman (SuperHero + HealingAbility)
    instantiation and method calls.

---
+++ fundamental/learn basic/OOP/realCode/main_2.java
@@ -0,0 +1,16 @@
+/**
+ * main_2
+ */
+public class main_2 {
+
+    public static void main(String[] args) {
+        Person Saifa = new Person();
+        Saifa.setName("Saifa");
+        Ironman tony = new Ironman();
+        tony.setName("Tony Katak");
+        tony.setAge(35);
+        tony.setJob("Super Star");
+        System.out.println(tony);
+        tony.heal();
+    }
+}

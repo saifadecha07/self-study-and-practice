1a63d81dfc3344efd73c3b5d361aa9c3b0d834af
Saifa Decha <saifadecha07@users.noreply.github.com>
2026-06-28 20:19:50 +0700

    feat: add abstract Student class and Person subclass
    Student defines abstract register() and test(). Person extends Student
    and implements both methods with university/ID assignment.

---
+++ fundamental/learn basic/OOP/realCode/Person.java
@@ -0,0 +1,13 @@
+/**
+ * Person
+ */
+public class Person extends Student{
+    public void register(){
+        setUniversity("Thammasat");
+         
+    }
+    public void test(){
+        setID("01");
+
+    }
+}
---
+++ fundamental/learn basic/OOP/realCode/Student.java
@@ -0,0 +1,25 @@
+/**
+ * Student
+ */
+public abstract class Student {
+    private String id;
+    private String name;
+    private String university;
+    
+    public abstract void register();
+    public abstract void test();
+
+    public Student(){
+
+    }
+
+    public void setID(String id){
+        this.name = name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setUniversity(String uni){
+        this.university = uni;
+    }
+}

48d72f75f347b8813c2361daed8f7769b2cc632c
Saifa Decha <saifadecha07@users.noreply.github.com>
2026-06-29 12:00:00 +0700

    feat: add naive solution for LeetCode Two Sum

---
+++ fundamental/pratice coding/java/TwoSum.java
@@ -0,0 +1,20 @@
+package fundamental.pratice_coding.java;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        TwoSum solution = new TwoSum();
+        int[] result = solution.twoSum(new int[]{2, 7, 11, 15}, 9);
+        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+    }
+}
